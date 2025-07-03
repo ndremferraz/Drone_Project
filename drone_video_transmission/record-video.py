9a95ca7aad6eabded69d13955cfb795eef3dc3b2
Andre <andreferraz@gwu.edu>
2025-07-03 17:34:50 -0400

    Inference over video stream implemented

--- drone_video_transmission/record-video.py
+++ drone_video_transmission/record-video.py
@@ -1,4 +1,5 @@
 import time, cv2
+from ultralytics import YOLO
 from threading import Thread
 from djitellopy import Tello
 
@@ -9,6 +10,8 @@ keepRecording = True
 tello.streamon()
 frame_read = tello.get_frame_read()
 
+model = YOLO('yolo11n_jul2_2025.pt')
+
 def video_stream():
     global keepRecording
     
@@ -18,21 +21,21 @@ def video_stream():
         
         if frame is not None and frame.size > 0:
             
-            cv2.imshow("Tello Live Stream", frame)
+            cv2.imshow("Tello Live Stream", model(frame)[0].plot())
             
         key = cv2.waitKey(1) & 0xFF
         
         if key == ord('q'):
             keepRecording = False
             
-        time.sleep(0.01)
+        time.sleep(1)
     cv2.destroyAllWindows()
 
 recorder = Thread(target=video_stream)
 recorder.start()
 
 try:
-    time.sleep(10)  
+    time.sleep(45)  
 finally:
     keepRecording = False
     recorder.join()
